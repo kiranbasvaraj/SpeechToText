99594b870158d5a3271df5a9594108837ce5f14f
kirankumar.b <kirankumar.b@cafecoffeeday.com>
2019-06-07 10:56:21 +0530

    Add project files.

---
+++ SpeechToText/SpeechToText.Android/DroidSpeechToText.cs
@@ -0,0 +1,25 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+using SpeechToText.Droid;
+using Xamarin.Forms;
+using Android.Speech;
+
+[assembly: Dependency(typeof(DroidSpeechToText))]
+namespace SpeechToText.Droid
+{
+    public class DroidSpeechToText : ConvertSpeechToText
+    {
+
+
+
+
+        public void Convert()
+        {
+            var contx = Forms.Context;
+            contx.StartActivity(typeof(RecordActivity));
+        }
+    }
+}
---
+++ SpeechToText/SpeechToText/ConvertSpeechToText.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeechToText
+{
+    public interface ConvertSpeechToText
+    {
+        void Convert();
+    }
+}
